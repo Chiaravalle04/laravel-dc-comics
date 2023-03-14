b3b5467d652e12466903523ba78e0f20d106d4cc
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-03-14 17:50:27 +0100

    add footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,37 @@
+<footer>
+
+    <div class="header-footer">
+
+        <div class="main-container d-flex jc-space-b ai-center">
+    
+            <div class="container-logo">
+    
+                <img src="{{ Vite::asset('resources/img/logo.png') }}" alt="">
+    
+            </div>
+
+            <div class="policy">
+
+                <a href="#">PRIVACY POLICY</a>
+
+                <a href="#">TERMINI E CONDIZIONI</a>
+
+                <hr>
+
+                <h5>Copyrigth ® DC COMICS</h5>
+
+            </div>
+    
+            <form action="#">
+    
+                <label for="newsletter">Iscriviti alla Newsletter</label>
+                <input type="email" name="newsletter" id="newsletter" placeholder="Inserisci la tua email...">
+                <button>ISCRIVITI</button>
+    
+            </form>
+    
+        </div>
+
+    </div>
+
+</footer>
